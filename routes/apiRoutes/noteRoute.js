78ebfd1a29bbab934f78fd9d7ca185e5b5024918
Pao Her <p.her007@hotmail.com>
2022-02-18 11:37:13 -0600

    fixed post route

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { count } = require('console');
+const {readFile} = require('fs');
 
 const notes = require('../../db/db.json');
 const createNewNote = require('../../lib/notes');
@@ -7,14 +9,25 @@ router.get('/notes', (req, res) => {
 
     res.json(notes);
 });
-
+// Develop/db/db.json
+// Develop/routes/apiRoutes/noteRoute.js
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+      
+   req.body.id = notes.length.toString();
 
     const note = createNewNote(req.body, notes);
     res.json(note);
 
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+    
+
 })
+
+
 
 
 
