c010d4914cf8a8152a4b4280261d149bd74d7ac5
Pao Her <p.her007@hotmail.com>
2022-02-18 01:29:22 -0600

    added GET and POST routes

---
+++ routes/apiRoutes/noteRoute.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const notes = require('../../db/db.json');
+const createNewNote = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
+})
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 
 app.use(express.static('public'));
@@ -8,6 +10,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
